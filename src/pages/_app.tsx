49ce8f8cf88c6c7979adc0a6e4e05615ac111317
iEXCEL-UNMC <85242160+iEXCEL-UNMC@users.noreply.github.com>
2022-08-09 13:32:08 -0500

    dsds

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -26,3 +26,16 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 };
 
 export default MyApp;
+
+// THIS IS THE CHAKRA UI NE
+import * as React from "react";
+// 1. import 'ChakraProvider' component
+import { ChakraProvider } from "@chakra-ui/react";
+function App() {
+  // 2. Use at the root of your app
+  return (
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  );
+}
